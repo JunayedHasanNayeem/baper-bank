9df1b3d947560b730f8fa47b39ceeda3090d5175
Junayed Hasan <junayedhasannayeem@gmail.com>
2021-08-19 01:08:15 +0600

    version1.0

---
+++ js/app.js
@@ -0,0 +1,191 @@
+//Common functions
+// Get Input Value
+ function getInputValue(inputID){
+    const inputElement = document.getElementById(inputID);
+    const inputValue = inputElement.value;
+    const inputAmount = parseFloat(inputValue);
+    inputElement.value = "";
+    return inputAmount;  
+ };
+
+// Deposit or Withdraw total amount
+ function getTotalAmount(previousValueID, newAmount){
+     const previousElement = document.getElementById(previousValueID);
+     const previousValue = previousElement.innerText;
+     const previousAmount = parseFloat(previousValue);
+     const totalAmount = previousAmount + newAmount;
+     
+     previousElement.innerText = totalAmount;
+     return previousElement;
+ };
+// Updated Total balance 
+ function getTotalBalance(totalBalanceID, newAmount){
+     const totalBalance = document.getElementById(totalBalanceID);
+     const previousTotalBalance = totalBalance.innerText;
+     const previousTotalAmount = parseFloat(previousTotalBalance);
+     const updatedTotalBalance = previousTotalAmount + newAmount;
+     totalBalance.innerText = updatedTotalBalance;
+     return totalBalance;
+ };
+// Validate Input
+function getErrorMessage(inputID, errorNotice){
+    const inputText = document.getElementById(inputID);
+    const inputBox = inputText.parentNode
+    const errorMessage = document.createElement("p");
+    errorMessage.className = "py-3 px-4 my-4 rounded bg-red-50";
+    errorMessage.innerText = errorNotice;
+    inputBox.prepend(errorMessage);
+};
+//Current Total balance
+function getCurrentBalance(currentBalanceID){
+    const totalBalance = document.getElementById(currentBalanceID);
+    const previousTotalBalance = totalBalance.innerText;
+    const previousTotalAmount = parseFloat(previousTotalBalance);
+    return previousTotalAmount;
+};
+
+//Add Deposits 
+document.getElementById('deposit-button').addEventListener('click', function(){
+    //Total deposit
+    const newDepositAmount = getInputValue('deposit-input');
+    if(newDepositAmount > 0){
+        getTotalAmount('deposit-total', newDepositAmount);
+        //Total Balance
+        getTotalBalance('balance-total', newDepositAmount);
+    }else{
+        getErrorMessage('deposit-input', 'Please enter a valid number!');
+    }
+    
+});
+
+//Add Withdraws
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    //Total Withdraw
+    const newWithdrawAmount = getInputValue('withdraw-input') ;
+    if(newWithdrawAmount > 0 &&  newWithdrawAmount <= getCurrentBalance('balance-total')){
+        getTotalAmount('withdraw-total', newWithdrawAmount);
+        //Total Balance
+        getTotalBalance('balance-total', -Math.abs(newWithdrawAmount));
+    }
+    else if(newWithdrawAmount >= getCurrentBalance('balance-total')){
+        getErrorMessage('withdraw-input', "Sorry, you don\'t have sufficient balance");
+    }
+    else{
+        getErrorMessage('withdraw-input', "Please enter a valid number!");
+        //getErrorMessage('withdraw-input', 'Please enter a valid amount');
+    }      
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /* //Error Message
+        const inputBox = inputElement.parentNode;
+        const errorMessage = document.createElement("p");
+        inputBox.prepend(errorMessage);
+        errorMessage.className = "py-3 px-4 my-4 rounded bg-red-50";
+        errorMessage.innerText = 'Please enter a valid amount!';
+        return errorMessage; */
+    
+        //Clear the number from the input field
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* //Baper App 
+
+//Add Deposits 
+document.getElementById('deposit-button').addEventListener('click', function(){
+    //Total deposit
+    const depositInput = document.getElementById('deposit-input');
+    const newDepositAmount = depositInput.value;
+    const depositTotal = document.getElementById('deposit-total');
+    const previousDepositAmount =  depositTotal.innerText;
+    const newDepositTotal = parseFloat( newDepositAmount) + parseFloat(previousDepositAmount);
+    depositTotal.innerText = newDepositTotal;
+    //Total Balance
+    let totalBalance = document.getElementById('balance-total');
+    const previousTotalBalance = totalBalance.innerText;
+    totalBalance.innerText = parseFloat(previousTotalBalance) + parseFloat(newDepositAmount);
+    //Clear the number from the input field
+    depositInput.value = "";
+   
+    
+})
+//Add Withdraws
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    const withdrawInput = document.getElementById('withdraw-input');
+    const newWithdrawAmount = withdrawInput.value;
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawAmount = withdrawTotal.innerText;
+    const newWithdrawTotal = parseFloat(previousWithdrawAmount) + parseFloat(newWithdrawAmount);
+    withdrawTotal.innerText = newWithdrawTotal; 
+   
+    //Total Balance
+    let totalBalance = document.getElementById('balance-total');
+    const previousTotalBalance = totalBalance.innerText;
+    totalBalance.innerText = parseFloat(previousTotalBalance) - parseFloat(newWithdrawAmount);
+
+    //Clear the input field 
+    withdrawInput.value = "";
+}) */
---
+++ js/login.js
@@ -0,0 +1,30 @@
+//Baper bank login
+document.getElementById('login-button').addEventListener('click', function(){
+    //Get User Email
+    const emailField = document.getElementById('login-email');
+    const loginEmail = emailField.value;
+    //Get User Password
+    const passwordField = document.getElementById('login-password');
+    const loginPassword = passwordField.value;
+    //Error Message
+    const loginForm = document.getElementById('login-from');
+    const errorMessage = document.createElement("p");
+    loginForm.prepend(errorMessage);
+    errorMessage.className = "py-3 px-4 my-4 rounded bg-red-50";
+    
+    
+
+    if(loginEmail=='sontan@baap.com' && loginPassword=='eww'){
+        location.href="baper-app.html"
+    }else if(loginEmail=='sontan@baap.com' && loginPassword!='eww'){
+        errorMessage.innerText = "Please enter a valid password!"
+    }else if(loginEmail!='sontan@baap.com' && loginPassword=='eww'){
+        errorMessage.innerText = "Please enter a valid email!"
+    }else{
+        errorMessage.innerText = "Please enter a valid email and password!"
+    };
+});
+
+
+
+
